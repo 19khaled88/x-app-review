6bd58049fca043ebe16e778e5a86ec211f0e91a0
19khaled88 <kkhaled88hasan@gmail.com>
2025-11-04 20:18:42 +0600

    allow dist to get uploaded to github

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,14 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// Home route 
+app.get('/',(req,res)=>{
+  try {
+    res.json({success:true,status:'OK',timestamp: new Date().toISOString()})
+  } catch (error) {
+    res.json({success:false,status:'201'})
+  }
+})
 // Routes
 app.get('/health', (req, res) => {
   res.json({ status: 'OK', timestamp: new Date().toISOString() });
